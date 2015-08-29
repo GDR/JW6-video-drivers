d7263844890d5806c1dd5dee0ef1cfb762bea18d
Damir Garifullin <gosugdr@gmail.com>
2015-08-30 01:04:51 +0300

    Initial commit

---
+++ video.sh
@@ -0,0 +1,44 @@
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+function print_message() {
+  echo -e $RED[---] $1$NC
+}
+
+print_message "Script which helps to install video drivers"
+if [ "$EUID" -ne 0 ]
+then
+  print_message "Plese run as root"
+  exit
+fi
+print_message "Installing main drivers"
+pacman -q --needed -S intel-dri xf86-video-intel  xf86-input-mouse xf86-input-synaptics xf86-input-keyboard
+print_message "Installing nvidia drivers"
+pacman -q --needed -S bumblebee nvidia bbswitch primus mesa-demos
+print_message "Adding to group bumblebee"
+gpasswd -a $(whoami) bumblebee
+print_message "Addiding bumblebee at startup"
+systemctl enable bumblebeed.service
+print_message "Adding to black list nouveau"
+echo "blacklist nouveau" > /etc/modprobe.d/modprobe.conf
+print_message "Editing bumblebee.conf"
+sed -i -e 's/Driver=/Driver=nvidia/g' /etc/bumblebee/bumblebee.conf
+sed -i -e 's/Bridge=auto/Bridge=virualgl/g' /etc/bumblebee/bumblebee.conf
+print_message "Editing mkinitcpio.conf"
+sed -i -e 's/MODULES=\"\"/MODULES=\"i915 bbswitch\"/g' /etc/mkinitcpio.conf
+print_message "Making initcpio"
+mkinitcpio -p linux
+print_message "Configuring grub"
+sed -i -e 's/GRUB_CMDLINE_LINUX_DEFAULT=\"quiet\"/GRUB_CMDLINE_LINUX_DEFAULT=\"rcutree.rcu_idle_gp_delay=1\"/g' /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+print_message "Fixing xorg"
+sed -i -e 's/#   BusID \"PCI:01:00:0\"/    BusID \"PCI:01:00:0\"/g' /etc/bumblebee/xorg.conf.nvidia
+print_message "Do you wish to reboot?"
+select yn in "Yes" "No"; do
+  case $yn in
+    Yes ) reboot; break;;
+    No ) exit;;
+  esac
+done
+exit
+
